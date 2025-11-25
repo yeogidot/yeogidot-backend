6b771570b3b23e402d8fb22d643ec2545aaac70a
Jeong Hee-cheol <113595306+hechul@users.noreply.github.com>
2025-11-25 19:13:15 +0900

    11/25

---
+++ src/main/java/com/yeogidot/yeogidot/entity/User.java
@@ -0,0 +1,30 @@
+package com.yeogidot.yeogidot.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @CreationTimestamp
+    @Column(name = "user_created", nullable = false, updatable = false)
+    private LocalDateTime userCreated;
+}
