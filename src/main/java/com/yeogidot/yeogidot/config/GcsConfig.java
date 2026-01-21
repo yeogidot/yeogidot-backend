6739948dd2569014ce4982e17975e315cb8f74b1
chikchok <tony09088@naver.com>
2026-01-22 00:30:38 +0900

    GCS 설정: credentials 경로를 동적으로 읽도록 수정

--- src/main/java/com/yeogidot/yeogidot/config/GcsConfig.java
+++ src/main/java/com/yeogidot/yeogidot/config/GcsConfig.java
@@ -7,7 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 
 import java.io.IOException;
 
@@ -15,22 +15,25 @@ import java.io.IOException;
  * Google Cloud Storage 설정
  */
 @Slf4j
-@Configuration // GCS 키 등록시 주석 해제
+@Configuration
 public class GcsConfig {
 
     @Value("${spring.cloud.gcp.project-id}")
     private String projectId;
+    
+    @Value("${spring.cloud.gcp.credentials.location}")
+    private Resource credentialsLocation;
 
     @Bean
     public Storage storage() throws IOException {
         log.info("🔧 GCS Storage Bean 초기화 시작");
-        log.info("📁 Credentials 위치: class path resource [gcs-key.json]");
+        log.info("📁 Credentials 위치: {}", credentialsLocation);
         log.info("🆔 Project ID: {}", projectId);
         
         Storage storage = StorageOptions.newBuilder()
                 .setProjectId(projectId)
                 .setCredentials(GoogleCredentials.fromStream(
-                        new ClassPathResource("gcs-key.json").getInputStream()))
+                        credentialsLocation.getInputStream()))
                 .build()
                 .getService();
         
