a70a00c2e54a1bb301bc93bea445f83ada0dadab
chikchok <tony09088@naver.com>
2026-01-27 15:58:05 +0900

    feat: TravelLog 벌크 삭제 쿼리 추가

--- src/main/java/com/yeogidot/yeogidot/repository/TravelLogRepository.java
+++ src/main/java/com/yeogidot/yeogidot/repository/TravelLogRepository.java
@@ -3,10 +3,15 @@ package com.yeogidot.yeogidot.repository;
 import com.yeogidot.yeogidot.entity.TravelDay;
 import com.yeogidot.yeogidot.entity.TravelLog;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TravelLogRepository extends JpaRepository<TravelLog, Long> {
-    void deleteByTravelDay(TravelDay travelDay);
-
+    
+    @Modifying
+    @Query("DELETE FROM TravelLog tl WHERE tl.travelDay = :travelDay")
+    void deleteByTravelDay(@Param("travelDay") TravelDay travelDay);
 }
